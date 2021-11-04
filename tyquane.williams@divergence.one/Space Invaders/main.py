56c678ef8435b48a0fe994777888a08d58bf0e73
tyquane <tyquane.williams@divergence.one>
2021-11-04 13:07:10 -0400

    Added screen color

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -17,3 +17,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+# RGB - Red, Green, Blue
+screen.fill((0, 0, 0,))
+pygame.display.update()

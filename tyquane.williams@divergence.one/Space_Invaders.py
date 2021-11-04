971e4a7fb34cc47766663456a27c3493adb9a7ca
tyquane <tyquane.williams@divergence.one>
2021-11-04 11:55:59 -0400

    Created the First Game Window

--- tyquane.williams@divergence.one/Space_Invaders.py
+++ tyquane.williams@divergence.one/Space_Invaders.py
@@ -1,4 +1,13 @@
-print("Space Invaders!")
+import pygame
 
+# Intialize the pygame
+pygame.init()
 
+# create the screen
+screen = pygame.display.set_mode((800,600))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False

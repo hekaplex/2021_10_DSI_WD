5eb233244cdf967269282d5f29f5b80819babbe4
tyquane <tyquane.williams@divergence.one>
2021-11-04 17:22:59 -0400

    Troubleshooting missing image

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -6,18 +6,33 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800,600))
 
-# Title and Icon
+# Caption and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.bilt(playerImg, (playerX, playerY))
+
+
 # Game Loop
 running = True
 while running:
+   
+    # RGB = Red, Green, Blue
+    screen.fill((0, 0, 0,))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-# RGB - Red, Green, Blue
-screen.fill((0, 0, 0,))
+
+
+player()
 pygame.display.update()

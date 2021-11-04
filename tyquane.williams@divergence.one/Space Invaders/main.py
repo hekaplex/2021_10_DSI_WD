bb331d1a7fe86fe606bd4c714b333e9bdfca591e
tyquane <tyquane.williams@divergence.one>
2021-11-04 12:38:03 -0400

    The file has been moved to a folder called Space Invaders

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -3,11 +3,13 @@ import pygame
 # Intialize the pygame
 pygame.init()
 
-# create the screen
+# Create the screen
 screen = pygame.display.set_mode((800,600))
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 
 # Game Loop
 running = True

a97376577f8e62474e10163cf7d7a575fa8defb4
tyquane <tyquane.williams@divergence.one>
2021-11-05 16:24:42 -0400

    Added movement to object

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -15,26 +15,35 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
-
-
+playerX_change = 0
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
+
 # Game Loop
 running = True
 while running:
-   
+    
     # RGB (Red, Green, Blue)
     screen.fill((0, 0, 0))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-# if keystroke is pressed check whether its right or left
-
-player(playerX,playerY)
-pygame.display.update()
-
+    
+        # if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0 
+        # 5 = 5 -0.1 -> 5 = 0.1
+        # 5 = 5 + 0.1
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update() 
 

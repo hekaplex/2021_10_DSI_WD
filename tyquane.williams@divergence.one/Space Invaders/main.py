fde7bea14b3a87149f4df30db92d733b2abe5fca
tyquane <tyquane.williams@divergence.one>
2021-11-05 18:10:12 -0400

    Added movement to the object

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -46,6 +46,12 @@ while running:
     # 5 = 5 + 0.1
     
     playerX += playerX_change
+    
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    
     player(playerX, playerY)
     pygame.display.update() 
 

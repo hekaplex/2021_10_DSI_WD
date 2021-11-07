7b4fef83b3647aed3462fbee00c2633839406340
tyquane <tyquane.williams@divergence.one>
2021-11-07 01:16:54 -0400

    Added the laser beams!

--- tyquane.williams@divergence.one/Space Invaders/main.py
+++ tyquane.williams@divergence.one/Space Invaders/main.py
@@ -28,12 +28,29 @@ enemyY = random.randint(50, 150)
 enemyX_change = 0.4
 enemyY_change = 40
 
+# Bullet
+
+# Ready - You can't see the bullet on the screen 
+# Fire - The bullet is currently moving
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 # Game Loop
@@ -54,6 +71,9 @@ while running:
                 playerX_change = -0.5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.5
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+        
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0 
@@ -79,6 +99,11 @@ while running:
     elif enemyX >= 736:
         enemyX_change = -0.4
         enemyY += enemyY_change
+
+    # Bullet Movement
+    if bullet_state is "fire": 
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)

3bbfd1bf56b44d5f3b6d7a4613ae40f6559b5656
Ryan H <hodgesjr0@gmail.com>
2021-11-15 18:09:12 -0500

    15 Nov Update

---
+++ Ryan_Hodges/Py/Book Exercises/Ch2E5.py
@@ -0,0 +1,25 @@
+'''
+Project 2-5: Travel Time Calculator
+Create a program that calculates the estimated hours and minutes for a trip.
+Console
+Travel Time Calculator
+Enter miles: 200
+Enter miles per hour: 65
+Estimated travel time
+Hours: 3
+Minutes: 5
+Specifications
+ The program should only accept integer entries like 200 and 65.
+ Assume that the user will enter valid data.
+Hint
+ Use integers with the integer division and modulus operators to get hours and
+minutes.
+'''
+
+milesToLocation = int(input("Miles to Destination: "))
+milesPerHour = int(input("Travel speed: "))
+hoursToDestination = milesToLocation//milesPerHour
+minutesToDestination = milesToLocation%milesPerHour
+
+print("\n Hours: \t" + str(hoursToDestination))
+print("\n Hours: \t" + str(minutesToDestination))

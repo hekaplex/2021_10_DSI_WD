e0751f52afa9bc92565e1584338c799c1ae14a70
Mike Zambeck <m.zambeck@gmail.com>
2021-11-16 16:57:16 -0600

    TMS - actually load one csv data file into table

--- mikezambeck/python/tms/main.py
+++ mikezambeck/python/tms/main.py
@@ -3,5 +3,6 @@ from tms_db import db_ops
 
 if __name__ == '__main__':
 
-    db_ops.m2add()
+    db_ops.m2_load()
+    # db_ops.m2add()
     print('fin')
--- mikezambeck/python/tms/tms_db/db_ops.py
+++ mikezambeck/python/tms/tms_db/db_ops.py
@@ -1,6 +1,7 @@
-from .database import session
+from .database import engine, session
 from .models import M2SL
 from datetime import datetime
+from sqlalchemy import text as text
 
 
 def m2add():
@@ -14,3 +15,18 @@ def m2add():
     session.commit()
 
 
+def m2_load():
+    # truncate table - no truncate in sqlite so use DELETE
+    engine.execute(text('DELETE FROM m2sl').execution_options(autocommit=True))
+    # load from csv
+    with open('./data/M2SL.csv') as fp:
+        lines = fp.readlines()[1:]
+        for line in lines:
+            date, data = line.split(',')
+            m2 = M2SL(
+                date=datetime.strptime(date, '%Y-%m-%d').date(),
+                m2sl = data
+            )
+            session.add(m2)
+
+    session.commit()

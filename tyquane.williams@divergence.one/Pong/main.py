9a41cd1a4026ce8973db90e8bb574fdc83929c18
tyquane <tyquane.williams@divergence.one>
2021-11-17 11:24:59 -0500

    Increased the size of the paddle

--- tyquane.williams@divergence.one/Pong/main.py
+++ tyquane.williams@divergence.one/Pong/main.py
@@ -10,6 +10,21 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+
+
+# Ball
+
+
 
 
 # Main Game Loop

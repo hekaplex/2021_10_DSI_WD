86b7f621a47e8cf9a5e39ce219958a7f30d8e10c
tyquane <tyquane.williams@divergence.one>
2021-11-17 11:27:26 -0500

    Created a second paddle

--- tyquane.williams@divergence.one/Pong/main.py
+++ tyquane.williams@divergence.one/Pong/main.py
@@ -20,7 +20,13 @@ paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
-
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
 

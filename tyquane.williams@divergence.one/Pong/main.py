37cb807cc36182cec67913de55926d2d4af51c82
tyquane <tyquane.williams@divergence.one>
2021-11-17 11:06:25 -0500

    Created the game window

---
+++ tyquane.williams@divergence.one/Pong/main.py
@@ -0,0 +1,17 @@
+#
+#
+#
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @TokyoEDTech")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+# Main Game Loop
+while True:
+    wn.update()

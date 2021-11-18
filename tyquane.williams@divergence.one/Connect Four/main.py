e7f70a6bea5c348a5e6baf1e903bd106c0336f4d
tyquane <tyquane.williams@divergence.one>
2021-11-18 13:16:43 -0500

    Created Game Window

--- tyquane.williams@divergence.one/Connect Four/main.py
+++ tyquane.williams@divergence.one/Connect Four/main.py
@@ -1,5 +1,9 @@
 import numpy as np # This is a module for Python. (This is not Pygame!)
+import pygame
+import sys
 
+BLUE = (0,0,255)
+BLACK = (0,0,0)
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
@@ -46,41 +50,72 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True 
 
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE(c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (c*SQUARESIZE+SQUARESIZE/2, r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2))
+
 
 board = create_board()
 print_board(board)
 game_over = False
 turn = 0
 
-while not game_over:
-    # Ask for Player 1 Input
-    if turn == 0:
-        col = int(input("Player 1 Make Your Selection (0-6):"))
 
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
+pygame.init() 
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+
+while not game_over:
     
-            if winning_move(board, 1): 
-                print("Player 1 Wins!")
-                game_over = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            continue
+            
+            # Ask for Player 1 Input
+            #if turn == 0:
+                #col = int(input("Player 1 Make Your Selection (0-6):"))
+
+                #if is_valid_location(board, col):
+                    #row = get_next_open_row(board, col)
+                    #drop_piece(board, row, col, 1)
+    
+                    #if winning_move(board, 1): 
+                        #print("Player 1 Wins!")
+                        #game_over = True
     
     
     # Ask for Player 2 Input
-    else:
-        col = int(input("Player 2 Make Your Selection (0-6):"))
+    #else:
+        #col = int(input("Player 2 Make Your Selection (0-6):"))
 
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 2)
+        #if is_valid_location(board, col):
+            #row = get_next_open_row(board, col)
+            #drop_piece(board, row, col, 2)
 
-            if winning_move(board, 2): 
-                print("Player 2 Wins!")
-                game_over = True
+            #if winning_move(board, 2): 
+                #print("Player 2 Wins!")
+                #game_over = True
 
-    print_board(board)
+    #print_board(board)
 
-    turn += 1
-    turn = turn % 2
+    #turn += 1
+    #turn = turn % 2
 
 

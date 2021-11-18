2a96ce046ea47a08241bedc9cdefc099d4b99189
Mike Zambeck <m.zambeck@gmail.com>
2021-11-17 22:23:22 -0600

    TMS - improve models, improve csv import, code cleanup

--- mikezambeck/python/tms/tms_db/db_ops.py
+++ mikezambeck/python/tms/tms_db/db_ops.py
@@ -1,19 +1,7 @@
-from .database import engine, session
-# from .models import M2SL
+from .database import session
 from .models import *
 from datetime import datetime
 from sqlalchemy import text as text
-
-
-def m2add():
-
-    m2 = M2SL(
-        date = datetime.strptime('2014-12-04', '%Y-%m-%d').date(),
-        m2sl = 125.9
-    )
-
-    session.add(m2)
-    session.commit()
 
 
 def m2_load():
@@ -43,12 +31,14 @@ def csv_load(tablename: str):
         lines = fp.readlines()[1:]
         for line in lines:
             date, data = line.split(',')
-            # string to class
-            m2 = globals()[tablename.upper()](
-                date=datetime.strptime(date, '%Y-%m-%d').date(),
-                value = data
-            )
-            session.add(m2)
+
+            # create model class from tablename
+            tbl = globals()[tablename.upper()]()
+            # assign class attributes (field data)
+            setattr(tbl, tbl.__table__.columns[0].name, datetime.strptime(date, '%Y-%m-%d').date())
+            setattr(tbl, tbl.__table__.columns[1].name, data)
+            # insert rec
+            session.add(tbl)
 
     session.commit()
 

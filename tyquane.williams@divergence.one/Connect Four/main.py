8325c8d0e7de7bb56d9ca376b2c4e4b239354624
tyquane <tyquane.williams@divergence.one>
2021-11-17 23:01:35 -0500

    Updated variable

--- tyquane.williams@divergence.one/Connect Four/main.py
+++ tyquane.williams@divergence.one/Connect Four/main.py
@@ -18,8 +18,11 @@ def get_next_open_row(board, col):
         if board[r][col] == 0:
             return r
 
+def print_board(board):
+    print(np.flip(board, 0))
+
 board = create_board()
-print(board)
+print_board(board)
 game_over = False
 turn = 0
 
@@ -40,7 +43,7 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
 
-    print(board)
+    print_board(board)
 
     turn += 1
     turn = turn % 2

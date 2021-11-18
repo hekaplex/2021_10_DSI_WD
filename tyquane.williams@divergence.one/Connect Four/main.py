119e2003ab99ce8cb148df8de52d08acb79a286d
tyquane <tyquane.williams@divergence.one>
2021-11-18 16:41:15 -0500

    Updated red and yellow circles

--- tyquane.williams@divergence.one/Connect Four/main.py
+++ tyquane.williams@divergence.one/Connect Four/main.py
@@ -95,6 +95,15 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
     
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
+
         if event.type == pygame.MOUSEBUTTONDOWN: 
             #print(event.pos)
             

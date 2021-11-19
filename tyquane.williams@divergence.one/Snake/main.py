71ca1a89371159397a159729ffef21eb9de6f3a2
tyquane <tyquane.williams@divergence.one>
2021-11-19 01:53:17 -0500

    Tested snake

--- tyquane.williams@divergence.one/Snake/main.py
+++ tyquane.williams@divergence.one/Snake/main.py
@@ -36,16 +36,20 @@ segments = []
 
 # Functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def move():
     if head.direction == "up":

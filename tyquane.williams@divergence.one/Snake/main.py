3858f359afb093d62f70e2c2c3e6dff1a8829b89
tyquane <tyquane.williams@divergence.one>
2021-11-18 23:28:50 -0500

    Created the game window

---
+++ tyquane.williams@divergence.one/Snake/main.py
@@ -0,0 +1,17 @@
+#
+# 
+# 
+
+import turtle
+
+# Screen Setup
+wn = turtle.Screen()
+wn.title("Snake") # This is where you change the Title 
+wn.bgcolor("green") # This is where you set the background color
+wn.setup(width=600, height=600) # This is where you set up the game dimensions
+wn.tracer(0) # Turns off animation on screen
+
+
+
+
+wn.mainloop()

afe76a4ff1c2e3100f6ce73bdbc52ac459c07841
tyquane <tyquane.williams@divergence.one>
2021-11-19 12:32:11 -0500

    Created the grid

--- tyquane.williams@divergence.one/Match Maker/main.py
+++ tyquane.williams@divergence.one/Match Maker/main.py
@@ -10,7 +10,7 @@ false = True
 previousx = 0
 previousy = 0
 
-button = {}
+buttons = {}
 button_symbols = {}
 
 symbols=[u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
@@ -19,6 +19,16 @@ symbols=[u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
          u'\u270C',u'\u270F',u'\u2712',u'\u2714',u'\u2716',u'\u2728']
 
 random.shuffle(symbols)
+
+
+for x in range(6):
+    for y in range(4):
+        button = Button(width=10, height=8)
+        button.grid(column=x, row=y)
+        buttons[x,y]=button
+        button_symbols[x,y]=symbols.pop()
+
+
 
 
 win.mainloop()

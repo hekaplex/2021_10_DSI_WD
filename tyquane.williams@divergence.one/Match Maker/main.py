153b0c45013cc27dccff9093cec06f775eb118b2
tyquane <tyquane.williams@divergence.one>
2021-11-19 12:55:15 -0500

    Corrected some syntax

--- tyquane.williams@divergence.one/Match Maker/main.py
+++ tyquane.williams@divergence.one/Match Maker/main.py
@@ -1,6 +1,37 @@
 import random
 import time
-from tkinter import Tk, Button, DISABLED # This is an imported library
+from tkinter import Tk, Button, DISABLED
+from tkinter.constants import TRUE # This is an imported library
+
+
+
+
+
+def show_symbol(x,y):
+    global first
+    global previousx, previousy
+    buttons[x,y]['text']=button_symbols[x,y]
+    buttons[x,y].update_idletasks()
+
+    if first:
+        previousx=x
+        previousy=y
+        first=False
+    elif previousx != x or previousy != y:
+        if buttons[previousx, previousy]['text']!= buttons[x,y]['text']:
+            time.sleep(0.5)
+            buttons[previousx, previousy]['text']= ' '
+            button[x,y]['text']=' '
+        else:
+            buttons[previousx, previousy]=DISABLED
+            buttons[x,y]=DISABLED
+        first = True
+
+
+
+
+
+
 
 
 win = Tk()
@@ -23,7 +54,7 @@ random.shuffle(symbols)
 
 for x in range(6):
     for y in range(4):
-        button = Button(width=10, height=8)
+        button = Button(command=show_symbol(x,y), width=10, height=8)
         button.grid(column=x, row=y)
         buttons[x,y]=button
         button_symbols[x,y]=symbols.pop()

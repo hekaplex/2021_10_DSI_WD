909b90a70a3eabc508d73e38a1ac348f235fdf31
tyquane <tyquane.williams@divergence.one>
2021-12-01 15:50:08 -0500

    Added a bird

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -15,6 +15,10 @@ floor_surface = pygame.image.load('Assets/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
+bird_surface = pygame.image.load('Assets/bluebird-midflap.png').convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (100,512))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,6 +26,7 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface,(0,0))
+    screen.blit(bird_surface,bird_rect)
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:

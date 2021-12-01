bcf2aa7077d14558798e947f229060b29414930b
tyquane <tyquane.williams@divergence.one>
2021-12-01 17:03:11 -0500

    Added moving pipes

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -8,6 +8,15 @@ def create_pipe():
     new_pipe = pipe_surface.get_rect(midtop = (288,512))
     return new_pipe
 
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        screen.blit(pipe_surface,pipe)
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
@@ -44,13 +53,20 @@ while True:
                 bird_movement -= 12
         if event.type == SPAWNPIPE:
             pipe_list.append(create_pipe())
-            print(pipe_list)
+            
 
     screen.blit(bg_surface,(0,0))
     
+    # Bird
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface,bird_rect)
+    
+    # Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+    # Floor
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:

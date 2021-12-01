e91b19a2a4c44b0724d8e9cd0f7787ff07903edc
tyquane <tyquane.williams@divergence.one>
2021-12-01 17:16:10 -0500

    Added top pipes

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -6,8 +6,9 @@ def draw_floor():
 
 def create_pipe():
     random_pipe_pos = random.choice(pipe_height)
-    new_pipe = pipe_surface.get_rect(midtop = (700,random_pipe_pos))
-    return new_pipe
+    bottom_pipe = pipe_surface.get_rect(midtop = (700,random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom = (700,random_pipe_pos - 300))
+    return bottom_pipe, top_pipe
 
 def move_pipes(pipes):
     for pipe in pipes:
@@ -54,7 +55,7 @@ while True:
                 bird_movement = 0
                 bird_movement -= 12
         if event.type == SPAWNPIPE:
-            pipe_list.append(create_pipe())
+            pipe_list.extend(create_pipe())
             
 
     screen.blit(bg_surface,(0,0))

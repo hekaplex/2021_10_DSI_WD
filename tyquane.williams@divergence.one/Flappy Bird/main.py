8fff2e83414bf01842e8b69eab61f9e84342845e
tyquane <tyquane.williams@divergence.one>
2021-12-01 00:26:26 -0500

    Created a looping floor

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -1,10 +1,19 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos,900))
+    screen.blit(floor_surface, (floor_x_pos + 576,900))
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('Assets/background-day.png')
+bg_surface = pygame.image.load('Assets/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load('Assets/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +22,11 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface,(0,0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
 
     pygame.display.update()
     clock.tick(120)

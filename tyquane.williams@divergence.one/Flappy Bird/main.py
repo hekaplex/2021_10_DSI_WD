ad5620d9aa61588948d516bebc7414791327685e
tyquane <tyquane.williams@divergence.one>
2021-12-01 16:53:53 -0500

    Added rectangles in the background

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -4,6 +4,10 @@ def draw_floor():
     screen.blit(floor_surface, (floor_x_pos,900))
     screen.blit(floor_surface, (floor_x_pos + 576,900))
 
+def create_pipe():
+    new_pipe = pipe_surface.get_rect(midtop = (288,512))
+    return new_pipe
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
@@ -23,6 +27,12 @@ bird_surface = pygame.image.load('Assets/bluebird-midflap.png').convert()
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (100,512))
 
+pipe_surface = pygame.image.load('Assets/pipe-green.png')
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE,1200)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,7 +42,9 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 12
-
+        if event.type == SPAWNPIPE:
+            pipe_list.append(create_pipe())
+            print(pipe_list)
 
     screen.blit(bg_surface,(0,0))
     

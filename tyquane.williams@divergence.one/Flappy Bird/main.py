1e528ab226f4564c48bbd7010426debaafb353ef
tyquane <tyquane.williams@divergence.one>
2021-11-30 21:42:22 -0500

    Updated window

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -1,5 +1,14 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
+clock = pygame.time.Clock()
 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(120)

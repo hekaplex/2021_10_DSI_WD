5b708db6e8acebeeefbb05a3a21948978c3f680e
tyquane <tyquane.williams@divergence.one>
2021-12-03 02:02:00 -0500

    Added High Score

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -41,11 +41,24 @@ def bird_animation():
     new_bird_rect = new_bird.get_rect(center = (100,bird_rect.centery))
     return new_bird, new_bird_rect
 
-def score_display():
-    score_surface = game_font.render('Score',True,(255,255,255)) # This is the score board with font and color
-    score_rect = score_surface.get_rect(center = (288,100))
-    screen.blit(score_surface,score_rect)
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)) ,True,(255,255,255)) # This is the score board with font and color
+        score_rect = score_surface.get_rect(center = (288,100))
+        screen.blit(score_surface,score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}' ,True,(255,255,255)) # This is the score board with font and color
+        score_rect = score_surface.get_rect(center = (288,100))
+        screen.blit(score_surface,score_rect)
 
+        high_score_surface = game_font.render(f'High Score: {int(high_score)}' ,True,(255,255,255)) # This is the score board with font and color
+        high_score_rect = high_score_surface.get_rect(center = (288,850))
+        screen.blit(high_score_surface,high_score_rect)
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
 
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
@@ -102,7 +115,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100,512)
                 bird_movement = 0
-
+                score = 0
         
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
@@ -128,7 +141,12 @@ while True:
         # Pipes
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
-        score_display()
+        
+        score += 0.01
+        score_display('main_game')
+    else:
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     # Floor
     floor_x_pos -= 1

464d213ca7c809aeb094c173b27c95de96707abd
tyquane <tyquane.williams@divergence.one>
2021-12-03 03:27:50 -0500

    Game Over

--- tyquane.williams@divergence.one/Flappy Bird/main.py
+++ tyquane.williams@divergence.one/Flappy Bird/main.py
@@ -13,7 +13,8 @@ def create_pipe():
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5
-    return pipes
+    visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    return visible_pipes
 
 def draw_pipes(pipes):
     for pipe in pipes:
@@ -24,12 +25,15 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe,pipe)
 
 def check_collision(pipes):
+    global can_score
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
+            can_score = True
             return False
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        can_score = True
         return False
     return True
 
@@ -61,7 +65,18 @@ def update_score(score, high_score):
         high_score = score
     return high_score
 
-pygame.mixer.pre_init(frequency=44100, size=16, channels=1, buffer=512)
+def pipe_score_check():
+    global score, can_score
+    
+    if pipe_list:
+        for pipe in pipe_list:
+            if 95 < pipe.centerx < 105 and can_score:
+                score += 1
+                score_sound.play()
+                can_score = False
+            if pipe.centerx < 0:
+                can_score = True
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
@@ -73,7 +88,7 @@ bird_movement = 0
 game_active = True
 score = 0
 high_score = 0
-
+can_score = True
 bg_surface = pygame.image.load('Assets/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 
@@ -149,12 +164,9 @@ while True:
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
         
-        score += 0.01
+        # Score
+        pipe_score_check()
         score_display('main_game')
-        score_sound_countdown -= 1
-        if score_sound_countdown <= 0:
-            score_sound.play()
-            score_sound_countdown = 100
     else:
         screen.blit(game_over_surface,game_over_rect)
         high_score = update_score(score, high_score)

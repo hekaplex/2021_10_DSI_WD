8e12f2a79f385a1f42623356842fbe69bb3680c1
tyquane <tyquane.williams@divergence.one>
2021-12-04 00:22:23 -0500

    Changed the background color

--- tyquane.williams@divergence.one/Tic Tac Toe/main.py
+++ tyquane.williams@divergence.one/Tic Tac Toe/main.py
@@ -11,14 +11,28 @@ screen_height = 300
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('TicTacToe')
 
+
+def draw_grid():
+    bg = (255, 255, 200)
+    grid = (50, 50, 50)
+    screen.fill(bg)
+    for x in range(1,3):
+        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100))
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_width))
+
+
 run = True
 while run:
+
+    draw_grid()
 
     # Add Event Handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+
+    pygame.display.update()
     
 pygame.quit()
 

35c92658778cf8701f0d79b4c7a16f2b8e94ca14
tyquane <tyquane.williams@divergence.one>
2021-12-04 00:15:57 -0500

    Created a game window

--- tyquane.williams@divergence.one/Tic Tac Toe/main.py
+++ tyquane.williams@divergence.one/Tic Tac Toe/main.py
@@ -1,5 +1,29 @@
 # Part 1
-#
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+screen_width = 300
+screen_height = 300
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('TicTacToe')
+
+run = True
+while run:
+
+    # Add Event Handlers
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    
+pygame.quit()
+
+
+
 #
 #
 #

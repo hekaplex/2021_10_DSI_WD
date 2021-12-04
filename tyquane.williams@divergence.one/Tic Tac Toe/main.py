4ebc8812106ae4bfa25a448af6bdcc69b1dd2597
tyquane <tyquane.williams@divergence.one>
2021-12-04 01:41:50 -0500

    Changed the grids width

--- tyquane.williams@divergence.one/Tic Tac Toe/main.py
+++ tyquane.williams@divergence.one/Tic Tac Toe/main.py
@@ -12,13 +12,16 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('TicTacToe')
 
 
+# Define Variables
+line_width = 6 # Thickness of the line
+
 def draw_grid():
     bg = (255, 255, 200)
     grid = (50, 50, 50)
     screen.fill(bg)
     for x in range(1,3):
-        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100))
-        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_width))
+        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100), line_width)
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_width), line_width)
 
 
 run = True

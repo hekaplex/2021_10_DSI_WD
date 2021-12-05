e67047f7ce27c187425a0b97dc529bafac244765
tyquane <tyquane.williams@divergence.one>
2021-12-04 23:51:45 -0500

    Displayed the background image

--- tyquane.williams@divergence.one/Battle/battle.py
+++ tyquane.williams@divergence.one/Battle/battle.py
@@ -10,13 +10,27 @@ screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Battle')
 
+
+# Load Images
+# Background Image
+background_img = pygame.image.load('Assets/Battle/Background/background.png').convert_alpha()
+
+# Function for drawing background
+def draw_bg():
+    screen.blit(background_img, (0, 0))
+
+
 run = True
 while run:
+
+    # Draw Background
+    draw_bg()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.display.update()
 
 pygame.quit()
 

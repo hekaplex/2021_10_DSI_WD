99860bdbd29417e0ab2ee9eff5529050400edad3
tyquane <tyquane.williams@divergence.one>
2021-12-04 23:58:44 -0500

    Seperated top and botton image

--- tyquane.williams@divergence.one/Battle/battle.py
+++ tyquane.williams@divergence.one/Battle/battle.py
@@ -15,10 +15,18 @@ pygame.display.set_caption('Battle')
 # Load Images
 # Background Image
 background_img = pygame.image.load('Assets/Battle/Background/background.png').convert_alpha()
+# Panel Image
+panel_img = pygame.image.load('Assets/Battle/Icons/panel.png').convert_alpha()
+
 
 # Function for drawing background
 def draw_bg():
     screen.blit(background_img, (0, 0))
+
+
+# Function for drawing panel
+def draw_panel():
+    screen.blit(panel_img, (0, screen_height - bottom_panel))
 
 
 run = True
@@ -26,6 +34,10 @@ while run:
 
     # Draw Background
     draw_bg()
+
+    # Draw Panel
+    draw_panel()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

6608deb2604b751d4c970bff2ebb99e53af4cc58
tyquane <tyquane.williams@divergence.one>
2021-12-04 23:53:24 -0500

    Increased the height of the game board

--- tyquane.williams@divergence.one/Battle/battle.py
+++ tyquane.williams@divergence.one/Battle/battle.py
@@ -4,8 +4,9 @@ pygame.init()
 
 
 # Game Window
+bottom_panel = 150
 screen_width = 800
-screen_height = 400
+screen_height = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Battle')

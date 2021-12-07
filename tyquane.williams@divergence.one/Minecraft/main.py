330a0cbac6e125a7397d7b3d58b83856f9ac6ca1
tyquane <tyquane.williams@divergence.one>
2021-12-07 01:33:11 -0500

    Created Minecraft Project

---
+++ tyquane.williams@divergence.one/Minecraft/main.py
@@ -0,0 +1,35 @@
+from ursina import *
+
+class Test_cube(Entity):
+    def __init__(self):
+        super().__init__(
+            model = 'cube',
+            color = color.white,
+            texture = 'white_cube',
+            rotation = Vec3(45, 45, 45)
+            )
+
+class Test_button(Button):
+    def __init__(self):
+        super().__init__(    
+            model = 'cube',
+            texture = 'brick',
+            color = color.blue)
+
+
+def update():
+    if held_keys['a']:
+        test_square.x -= 4 * time.dt
+
+
+app = Ursina()
+
+test_square = Entity(model = 'quad', color = color.red, scale = (1, 4), position = (5,4))
+
+sans_texture = load_texture('Assets/Minecraft/Sans.png')
+sans = Entity(model = 'quad', texture = sans_texture)
+
+test_cube = Test_button()
+
+app.run()
+

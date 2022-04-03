925f1ce703cce539ffd641e389a0fc7bf35a330e
Kate Knowles Murphy <91904581+KateKnowlesMurphy@users.noreply.github.com>
2022-04-03 14:47:32 -0400

    Add files via upload

---
+++ kate.knowles.murphy@divergence.one/python/eligibility boolean.py
@@ -0,0 +1,9 @@
+def eligibility():
+    major = input("What is your major? ")
+    major_input = ([math, computer science, physics, biology, geology, chemistry])
+    if major (math, computer science, physics) is TRUE and not (biology, geology, chemistry)
+        print ("You are eligible to apply for the scholarship.")
+    if major = (biology, geology, chemistry) is TRUE and not (math, computer science, physics)
+        print ("Sorry, you are ineligible for this scholarship.")
+        
+eligiblity()
---
+++ kate.knowles.murphy@divergence.one/python/state inputs valid elif.py
@@ -0,0 +1,10 @@
+def state():
+    abbState=input(“Enter a two letter abbreviation for a U.S. state:  ")
+    if abbState.len(2)
+        print(“Your input is valid.”)
+    if abbState = ("FL" or "fl" or "Fl" or "fl")
+        print("Florida is my favorite state!")
+    elif>or<abbState.len(2)
+        print("Your input is not valid.”)
+state()
+
